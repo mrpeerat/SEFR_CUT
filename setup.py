9177a90c0b99d8a024a7b28afbbd1b357685972f
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-10-05 21:50:03 +0700

    SEFR_CUT v1.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages = ['sefr_cut','sefr_cut.deepcut','sefr_cut.model','sefr_cut.variable','sefr_cut.weight','sefr_cut.deepcut.weight'],
     include_package_data = True,
     package_data={"sefr_cut": ['model/*','variable/*','weight/*','deepcut/weight/*']},
-    version = '1.0',
+    version = '1.1',
     install_requires = requirements,
     long_description = readme,
     long_description_content_type='text/markdown',
@@ -32,8 +32,11 @@ setup(
     author_email = 'peerat.l_s19@vistec.ac.th',
     url = 'https://github.com/mrpeerat/SEFR_CUT',
     download_url = '',
-    keywords = ['thai word segmentation deep learning machine learning neural network development'],
+    keywords = ['thai word segmentation', 'word segmentation', 'thainlp'],
     classifiers = [
-        'Development Status :: 3 - Alpha'
+        'Development Status :: 5 - Production/Stable',
+        'Natural Language :: Thai',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Text Processing :: Linguistic'
     ],
 )
